205c41fe2283c02d8065004d24a0ad234454f39a
Bervyn Co <bervyn.co@jws.com.ph>
2024-09-30 16:58:35 +0800

    Disable ssl

--- config/database.php
+++ config/database.php
@@ -44,7 +44,6 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -58,7 +57,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => 'isrgrootx1.pem'
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
