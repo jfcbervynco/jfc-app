1ab6ab60c83823f8002114107ef7c24c368ab8d5
Bervyn Co <bervyn.co@jws.com.ph>
2024-09-30 17:00:47 +0800

    Enable pem

--- config/database.php
+++ config/database.php
@@ -56,8 +56,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'sslmode' => 'require',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => 'isrgrootx1.pem'
             ]) : [],
         ],
 
